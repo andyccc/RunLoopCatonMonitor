57e7eb339131e7493f88c6d3d83d697f719ed5ec
world <aoyan2010@live.cn>
2017-01-20 14:38:43 +0800

    增加卡顿检测方法

---
+++ MonitorTimer/MonitorTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MonitorTimer
+//
+//  Created by wd on 2019/6/28.
+//  Copyright © 2019 wd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MonitorTimer/MonitorTimer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MonitorTimer
+//
+//  Created by wd on 2019/6/28.
+//  Copyright © 2019 wd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
