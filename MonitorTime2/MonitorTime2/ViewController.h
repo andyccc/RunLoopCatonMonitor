366ee21058ea7574ee48962e91e547103ce69b14
world <aoyan2010@live.cn>
2017-02-10 14:39:10 +0800

    增加卡顿检测方法

---
+++ MonitorTime2/MonitorTime2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MonitorTime2
+//
+//  Created by wd on 2019/7/1.
+//  Copyright © 2019 wd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
